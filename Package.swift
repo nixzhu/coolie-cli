a4aacafe03183f55bc4efb15574d6759fd1f60a0
nixzhu <zhuhongxu@gmail.com>
2017-02-08 11:54:20 +0800

    inc Coolie version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "coolie-cli",
     dependencies: [
-        .Package(url: "https://github.com/nixzhu/Coolie.git", "0.7.0")
+        .Package(url: "https://github.com/nixzhu/Coolie.git", "0.8.0")
     ]
 )
